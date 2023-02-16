4fa0fb5c7a0435da208519bda90c5e7bbf62cb2f
Gonenc Baris Bezik <112894114+gncbrs@users.noreply.github.com>
2023-02-16 18:29:36 +0300

    Update MATH_GAME.java

--- MATH_GAME.java
+++ MATH_GAME.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Math_Game {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
